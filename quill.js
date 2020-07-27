849c91cad10fd1786cf2bd64de8fdebbbae13519
PatrickHaussmann <36867676+PatrickHaussmann@users.noreply.github.com>
2020-07-27 14:15:02 +0200

    home in toolbar as link

--- quill.js
+++ quill.js
@@ -168,9 +168,6 @@ let quillOptions = {
                 },
                 'redo': function() {
                     quill.history.redo()
-                },
-                'home': function(){
-                    location.href = location.origin
                 }
             }
         },
